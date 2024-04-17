5dd54c8d0d4b30bbcbd0a09c6dd74a9e8ca7e088
Vilppu Saarinen <vilppu.saarinen@reaktor.com>
2024-04-17 13:34:30 +0300

    fix: change format of errors to allow returning several form errors at once

--- events/api.py
+++ events/api.py
@@ -1582,25 +1582,31 @@ class EventSerializer(BulkSerializerMixin, EditableLinkedEventsObjectSerializer,
         start_time = validated_data.get('start_time', None)
         end_time = validated_data.get('end_time', None)
         date_published = validated_data.get('date_published', None)
-        super_event = validated_data.pop('super_event', None)
+        validation_errors = {}
 
         if instance.super_event is not None:
-            if ((instance.super_event.start_time
-                 and start_time is not None
-                 and start_time < instance.super_event.start_time)
-                or (instance.super_event.end_time
-                    and end_time is not None
-                    and end_time > instance.super_event.end_time)):
-                raise serializers.ValidationError('TIME_MISMATCH_ERROR')
+            if (
+                    instance.super_event.start_time and
+                    start_time is not None and
+                    start_time < instance.super_event.start_time
+            ):
+                validation_errors['start_time'] = ['TIME_MISMATCH_ERROR']
+            if (
+                    instance.super_event.end_time and
+                    end_time is not None and
+                    end_time > instance.super_event.end_time
+            ):
+                validation_errors['end_time'] = ['TIME_MISMATCH_ERROR']
             if (
                     instance.super_event.date_published
                     and date_published
                     and date_published < instance.super_event.date_published
             ):
-                raise serializers.ValidationError('DATE_PUBLISHED_INVALID_ERROR')
+                validation_errors['start_time'].append('DATE_PUBLISHED_INVALID_ERROR')
         if instance.end_time and instance.end_time < timezone.now() and not self.data_source.edit_past_events:
-            raise serializers.ValidationError('PAST_EVENT_EDIT_ERROR')
-
+            validation_errors['start_time'].append('PAST_EVENT_EDIT_ERROR')
+        if validation_errors != {}:
+            raise serializers.ValidationError(validation_errors)
         # The API only allows scheduling and cancelling events.
         # POSTPONED and RESCHEDULED may not be set, but should be allowed in already set instances.
         if validated_data.get('event_status') in (Event.Status.POSTPONED, Event.Status.RESCHEDULED):
@@ -1640,6 +1646,7 @@ class EventSerializer(BulkSerializerMixin, EditableLinkedEventsObjectSerializer,
                 validated_data.pop(field.source)
 
         # update validated fields
+        super_event = validated_data.pop('super_event', None)
         if None is not super_event:
             validated_data['super_event'] = super_event
         super().update(instance, validated_data)
